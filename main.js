1f654871ed3915d6f55d239afda3cb755653f084
Electro6896 <82365662+Electro6896@users.noreply.github.com>
2021-07-16 13:08:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("my first console project");
+
+var first_name="amlan ";
+var last_name="panda ";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a=10;
+var b=5;
+
+var add=a+b;
+console.log(add);
+
+var sub=a-b;
+console.log(sub);
+
+var mul=a*b;
+console.log(mul);
+
+
+var div=a/b;
+console.log(div);
